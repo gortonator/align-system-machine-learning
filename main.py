905c7cceda28750409c3fa88b4b583a6b9a67809
Nicholas Carugati <carugati.n@husky.neu.edu>
2018-02-19 23:12:51 -0500

    Implemented Basic Emailing for Unknown Queries
    Still needs plenty of work done

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 from flask import request, jsonify
 import requests
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import getpass
 
+username = raw_input('Enter your email username: ')
+password = getpass.getpass(prompt='Enter your email password: ')
 app = Flask(__name__)
-
 
 @app.route("/")
 def main():
@@ -13,7 +18,12 @@ def main():
 @app.route("/api")
 def invoke_question():
     query = request.args.get('query', 'Basic Question', type=str)
-    return jsonify(get_answer(query))
+    payload = get_answer(query)
+    query_decision = payload['result']['action']
+    if query_decision == "input.unknown":
+        send_email(query)
+    answer = {"question": query, "answer": payload['result']['fulfillment']['speech']}
+    return jsonify(answer)
 
 
 def get_answer(question):
@@ -24,4 +34,16 @@ def get_answer(question):
     return req.json()
 
 
+def send_email(question):
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(username, password)
 
+    msg = MIMEMultipart()
+    msg['Subject'] = "You have a new unanswered question!"
+    msg['From'] = username
+    msg['To'] = username
+    text = "You have a question to answer: \n\n" + question
+    msg.attach(MIMEText(text, 'plain'))
+    s.sendmail(username, username, msg.as_string())
+    s.quit()
