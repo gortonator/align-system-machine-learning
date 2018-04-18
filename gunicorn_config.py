c329f4aa8da906db5c9587f762cd2698a0e4a969
Rahul Thomas <thomas.rah@husky.neu.edu>
2018-04-17 21:03:00 -0700

    Added gunicorn config

---
+++ gunicorn_config.py
@@ -0,0 +1,14 @@
+import multiprocessing
+
+bind = "0.0.0.0:5000"
+backlog = 1024
+
+timeout = 30
+keepalive = 3
+
+worker_class = "gthread"
+workers = multiprocessing.cpu_count() * 2 + 1
+threads = multiprocessing.cpu_count() * 2
+
+log_level = "critical"
+proc_name = "gunalignbot"
